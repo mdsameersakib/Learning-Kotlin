4cd34cf68a4f7a51d724d79718bba11670e13a91
Md. Sameer Sakib <mdsameersakib@gmail.com>
2024-10-10 14:15:05 +0600

    Create type_conversion.kt

---
+++ type_conversion.kt
@@ -0,0 +1,6 @@
+fun main() {
+
+  val userinp = readLine()!!.toInt() // As readLine takes input as string this will convert it to integer. 
+  val userinp = readLine()!!.toFloat() // As readLine takes input as string this will convert it to Float. 
+
+}
